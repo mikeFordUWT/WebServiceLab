88a80c75626892c28451e0cae62b9773db549db1
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-05-09 14:14:31 -0700

    Testing lab in process

--- app/src/main/java/fordm13/tacoma/uw/edu/webservicelab/model/Course.java
+++ app/src/main/java/fordm13/tacoma/uw/edu/webservicelab/model/Course.java
@@ -29,9 +29,11 @@ public class Course implements Serializable {
     }
 
     public void setCourseId(String courseId) throws IllegalArgumentException{
-        if(courseId== null || courseId.length() <5){
+        if(courseId.length() <5){
             throw new IllegalArgumentException("courseId must be >= 5 long");
 
+        }else if(courseId== null ){
+            throw new IllegalArgumentException("You must enter a courseID");
         }else{
             this.mCourseId = courseId;
         }
